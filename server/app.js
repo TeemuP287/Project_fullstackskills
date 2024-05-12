b89ba332988297807a1aea5851f59bf2be884899
TeemuP287 <61014813+TeemuP287@users.noreply.github.com>
2024-05-12 13:28:03 +0300

    gettaskfrommongodb

--- server/app.js
+++ server/app.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // Tuo cors-moduuli
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = mongoose.connection;
 const taskRoutes = require('./routes/taskRoutes'); // Ota taskRoutes käyttöön
 
+app.use(cors()); // Käytä cors-middlewarea sallimaan CORS-pyynnöt
 app.use(express.json());
 app.use('/api/tasks', taskRoutes); // Liitä taskRoutes reitit sovellukseen
-// Connect to MongoDB database
 
-mongoose.connect('mongodb://localhost:27017/myDatabase', {
+// Connect to MongoDB database
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-
 });
 
 // Check for successful connection
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => 
-{
+db.once('open', () => {
   console.log('Connected to MongoDB database');
-
 });
 
 // Define your routes and other server logic here
-
 app.listen(PORT, () => {
   console.log(`Express server running on port ${PORT}`);
 });
