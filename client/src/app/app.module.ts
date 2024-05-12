7bf2358a22348e7f5b0c986d5a614551f5275f63
TeemuP287 <61014813+TeemuP287@users.noreply.github.com>
2024-05-12 07:23:26 +0300

    appcompts, appcomphtml,appmodulets

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskService } from './services/task.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent
+    // Lisää muita komponentteja tänne tarpeen mukaan
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+    // Lisää muita Angular-moduuleja tänne tarpeen mukaan
+  ],
+  providers: [
+    TaskService
+    // Lisää muita palveluita tänne tarpeen mukaan
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
