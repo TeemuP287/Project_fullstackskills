d58d7fe2f6dfdd0144cd18ba29abd7d38e3cba6a
TeemuP287 <61014813+TeemuP287@users.noreply.github.com>
2024-05-12 06:52:19 +0300

    taskjsedit

--- client/src/app/components/task-list/task-list.component.ts
+++ client/src/app/components/task-list/task-list.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/Task';
 
 @Component({
   selector: 'app-task-list',
-  standalone: true,
-  imports: [],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.getTasks();
+  }
+
+  getTasks(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  deleteTask(task: Task): void {
+    this.taskService.deleteTask(task.id).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id);
+    });
+  }
+
+  toggleCompleted(task: Task): void {
+    task.completed = !task.completed;
+    this.taskService.updateTask(task).subscribe();
+  }
 }
+
